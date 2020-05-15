19f020bef94aa8caa482ba7ff9f9e72023290c6b
Andrew Volostnykh <zighalt790@gmail.com>
2020-05-15 10:05:40 +0300

    All functions ready(waiting for test)

--- src/javascript/fight.js
+++ src/javascript/fight.js
@@ -1,16 +1,43 @@
 export function fight(firstFighter, secondFighter) {
   // return winner
+  let firstHealth = firstFighter.health;
+  let secondHealth = secondFighter.health;
+
+  while(true){
+
+    if(firstHealth <= 0){
+      return secondFighter;
+    } else if(secondHealth <= 0){
+      return firstFighter;
+    }
+
+    let damageForSecond = getDamage(firstFighter, secondFighter);
+    secondHealth = secondHealth - damageForSecond;
+
+    let damageForFirst = getDamage(secondFighter, firstFighter);
+    firstHealth = firstHealth - damageForFirst;
+
+  }
 }
 
 export function getDamage(attacker, enemy) {
   // damage = hit - block
   // return damage 
+  const damage = getHitPower(attacker) - getBlockPower(enemy);
+  return damage;
 }
 
 export function getHitPower(fighter) {
-  // return hit power
+  const power = fighter.attack * chance();
+  return power;
 }
 
 export function getBlockPower(fighter) {
-  // return block power
+  const power = fighter.defense * chance();
+  return power;
+}
+
+// this function returning random number - 1 or 2;
+function chance(){
+  return 1 + Math.floor(Math.random() * Math.floor(2))
 }
